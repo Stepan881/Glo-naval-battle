1333e1f2ba24e51cb5df98799565bd5d70c4f78d
stepan881 <stepanstepanov180@gmail.com>
2020-03-06 10:13:57 +0300

    Третий урок

--- script.js
+++ script.js
@@ -7,25 +7,89 @@ const again = document.querySelector('#again');
 const header = document.querySelector('.header');
 
 const game = {
-    ships: [
-        {
-            location: ['26', '36', '46', '56'],
-            hit: ['', '', '', '']
-        }, 
-        {
-            location: ['11', '12', '13'],
-            hit: ['', '', '']
-        }, 
-        {
-            location: ['69', '79'],
-            hit: ['', '']
-        }, 
-        {
-            location: ['32'],
-            hit: ['']
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+
+    collision: new Set(),
+
+    generateShips() {
+        for (let i = 0; i < this.optionShip.count.length; i++) {
+            for (let k = 0; k < this.optionShip.count[i]; k++) {
+                const size = this.optionShip.size[i];
+                const ship = this.generateOprionsShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+                
+            }  
         }
-    ],
-    shipCount: 4,
+    },
+    generateOprionsShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: [],
+        };
+
+        const derecton = Math.random() < 0.5;
+        let x, y;
+
+        if (derecton) {
+            x = Math.floor(Math.random() * 10); 
+            y = Math.floor(Math.random() * (10 - shipSize)); 
+        } else {
+            x = Math.floor(Math.random() * (10 - shipSize)); 
+            y = Math.floor(Math.random() * (10)); 
+        }
+
+        for (let i = 0; i < shipSize; i++) {
+            if (derecton) {
+                ship.location.push(x + '' + (y + i));
+            } else {
+                ship.location.push((x + i) + '' + y);
+            }
+            ship.hit.push('');
+        } 
+
+        if (this.checkCollision(ship.location)) {
+            return this.generateOprionsShip(shipSize);
+        }
+
+        this.addCollision(ship.location);
+
+        return ship;
+    },
+    checkCollision(location) {
+        for (const coord of location) {
+            if (this.collision.has(coord)) {
+                return true;
+            }
+        }
+    },
+
+    addCollision(location) {
+        for (let i = 0; i < location.length; i++) {
+            const startCooedX = location[i][0] - 1;
+
+            for (let j = startCooedX; j < startCooedX + 3; j++) {
+                 const startCoordY =  location[i][1] - 1;
+
+                 for (let z = startCoordY; z < startCoordY + 3; z++) {
+
+                    if (j >= 0 && j < 10 && z >= 0 && z < 10) {
+                        const coord = j + '' + z;
+                        this.collision.add(coord);
+                        
+                    }
+
+                     
+                 }
+            }
+
+        }
+    }
 }
 
 
@@ -64,7 +128,9 @@ const show = {
 const fire = (event) => {
     const target = event.target;
     
-    if (target.classList.length !== 0 || target.tagName !== 'TD' || game.shipCount === 0) return;
+    if (target.classList.length !== 0 ||
+         target.tagName !== 'TD' ||
+          !game.shipCount) return;
     show.miss(target);
     play.updateData = 'shot';
    
@@ -84,7 +150,7 @@ const fire = (event) => {
 
                 game.shipCount -= 1;
 
-                if (game.shipCount < 1) {
+                if (!game.shipCount) {
                     header.textContent = 'End Game';
                     header.style.color = 'red';
 
@@ -102,10 +168,18 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
-
+    game.generateShips();
+    
     again.addEventListener('click', () => {
         location.reload();
     });
+
+    record.addEventListener('dblclick', () => {
+        localStorage.clear();
+        play.record = 0;
+        play.render();
+    });
+    
 }
 
 init();
